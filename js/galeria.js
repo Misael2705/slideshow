a01e2996273bbf4049669459fc2814c6b2a542bf
MISAEL <patricio.mayo.26@gmail.com>
2025-06-19 22:48:23 -0600

    cambios

--- js/galeria.js
+++ js/galeria.js
@@ -8,7 +8,8 @@ var pg = {
     rutaImagen: null,
     cuerpoDom: document.querySelector("body"),
     lightbox: null,
-    modal: null
+    modal: null,
+    animacionGaleria: "fade"
 }
 
 /*=============================================
@@ -35,7 +36,7 @@ var mg = {
 
     lightbox: function(img){
 
-        pg.cuerpoDom.appendChild(document.createElement("div")).setAttribute("id", "lightbox");
+        pg.cuerpoDom.appendChild(document.createElement("DIV")).setAttribute("id", "lightbox");
         pg.lightbox = document.querySelector("#lightbox");
 
         pg.lightbox.style.width = "100%";
@@ -46,23 +47,77 @@ var mg = {
         pg.lightbox.style.top = 0;
         pg.lightbox.style.lefts = 0;
 
-        pg.lightbox.appendChild(document.createElement("div")).setAttribute("id", "modal");
+        pg.lightbox.appendChild(document.createElement("DIV")).setAttribute("id", "modal");
 
         pg.modal = document.querySelector("#modal");
 
-        pg.modal.innerHTML = img.outerHTML + "<div>X</div>";
-
-        pg.modal.childNodes[0].style.width = "100%";
-        pg.modal.childNodes[0].style.border = "15px solid white";
+        pg.modal.innerHTML = img.outerHTML + "<div class='cerrar'>x</div>";
 
         pg.modal.style.display = "block";
         pg.modal.style.position = "relative";
-        pg.modal.style.width = "60%";
-        pg.modal.style.top = "50%";
-        pg.modal.style.left = "50%";
-        pg.modal.style.marginLeft = -pg.modal.childNodes[0].width/2 + "px";
-        pg.modal.style.marginTop = -pg.modal.childNodes[0].height/2 + "px";
+        pg.modal.childNodes[0].style.width = "100%";
+        pg.modal.childNodes[0].style.border = "15px solid white";
 
+        if (window.matchMedia("(max-width:1000px)").matches) {
+        
+            pg.modal.style.width = "90%";
+
+        } else {
+
+            pg.modal.style.width = "60%";
+
+        }        
+        
+        if (pg.animacionGaleria == "slideLeft") {
+
+            pg.modal.style.top = "50%";
+            pg.modal.style.left = 0;
+            pg.modal.style.opacity = 0;
+
+            setTimeout(function (){
+
+                pg.modal.style.transition = ".5s left ease";
+                pg.modal.style.left = "50%";
+                pg.modal.style.opacity = 1;
+                pg.modal.style.marginLeft = -pg.modal.childNodes[0].width/2 + "px";
+                pg.modal.style.marginTop = -pg.modal.childNodes[0].height/2 + "px";
+
+            }, 50)
+        }
+
+        if (pg.animacionGaleria == "slideTop") {
+
+            pg.modal.style.top = "-100%";
+            pg.modal.style.left = "50%";
+            pg.modal.style.opacity = 0;
+
+            setTimeout(function (){
+
+                pg.modal.style.transition = ".5s top ease";
+                pg.modal.style.top = "50%";
+                pg.modal.style.opacity = 1;
+                pg.modal.style.marginLeft = -pg.modal.childNodes[0].width/2 + "px";
+                pg.modal.style.marginTop = -pg.modal.childNodes[0].height/2 + "px";
+
+            }, 50)
+        }
+
+        if (pg.animacionGaleria == "fade") {
+
+            pg.modal.style.top = "50%";
+            pg.modal.style.left = "50%";
+            pg.modal.style.opacity = 0;
+
+            setTimeout(function (){
+
+                pg.modal.style.transition = ".5s opacity ease";
+                pg.modal.style.opacity = 1;
+                pg.modal.style.marginLeft = -pg.modal.childNodes[0].width/2 + "px";
+                pg.modal.style.marginTop = -pg.modal.childNodes[0].height/2 + "px";
+
+            }, 50)
+        }
+        
 
         pg.modal.childNodes[1].style.position = "absolute";
         pg.modal.childNodes[1].style.right = "5px";
@@ -75,6 +130,13 @@ var mg = {
         pg.modal.childNodes[1].style.textAling = "center";
         pg.modal.childNodes[1].style.background = "white";
         pg.modal.childNodes[1].style.borderRadius = "0px 0px 0px 5px";
+        
+        pg.modal.childNodes[1].addEventListener("click", mg.salirGaleria)
+    },
+
+    salirGaleria: function() {
+
+        pg.lightbox.parentNode.removeChild(pg.lightbox);
         
     }
 }
